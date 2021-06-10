5547ce7a4843d8c13a1064957001cdff76f8ccb0
alexbasista <abasista@hashicorp.com>
2021-06-10 10:18:51 -0400

    update test

--- tests/with-vcs/tf-working-dir-test/test_main.tf
+++ tests/with-vcs/tf-working-dir-test/test_main.tf
@@ -1,10 +1,3 @@
-# resource "random_shuffle" "teststring" {
-#     input = [
-#         for stark in var.starks:
-#         lower(stark)
-#     ]
-# }
-
 resource "random_shuffle" "testlist" {
     input = [
         for i in var.testlist:
