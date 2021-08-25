18ddd6b6cb1127e6247d89dcc27623ba0469e86d
alexbasista <abasista@hashicorp.com>
2021-08-25 13:11:41 -0400

    update with-vcs test scenario

--- tests/with-vcs/tf-working-dir-test/test_outputs.tf
+++ tests/with-vcs/tf-working-dir-test/test_outputs.tf
@@ -1,15 +1,15 @@
-output "teststring" {
-    value = random_pet.teststring.id
-}
+# output "teststring" {
+#     value = random_pet.teststring.id
+# }
 
-output "secretstring" {
-    value = random_pet.secstring.id
-}
+# output "secretstring" {
+#     value = random_pet.secstring.id
+# }
 
-output "testlist" {
-    value = random_shuffle.testlist.result
-}
+# output "testlist" {
+#     value = random_shuffle.testlist.result
+# }
 
-output "secretlist" {
-    value = random_shuffle.seclist.result
-}
+# output "secretlist" {
+#     value = random_shuffle.seclist.result
+# }
--- tests/with-vcs/variables.tf
+++ tests/with-vcs/variables.tf
@@ -1,5 +1,9 @@
+variable "tfe_hostname" {
+  type        = string
+  description = "Hostname of TFE instance to provision against."
+}
+
 variable "oauth_token_id" {
     type      = string
     sensitive = true
-    default   = null
 }
