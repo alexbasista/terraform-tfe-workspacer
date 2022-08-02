2e6fad6a18ed76ed1785b8c8f4371560cc044aad
alexbasista <abasista@hashicorp.com>
2022-08-01 20:22:08 -0400

    code

---
+++ variable_sets.tf
@@ -0,0 +1,13 @@
+data "tfe_variable_set" "vs" {
+  for_each = toset(var.variable_set_names)
+  
+  name         = each.value
+  organization = var.organization
+}
+
+resource "tfe_workspace_variable_set" "vs" {
+  for_each = data.tfe_variable_set.vs
+  
+  variable_set_id = each.value.id
+  workspace_id    = tfe_workspace.ws.id
+}
