ee6674e2836b68185cb5e6325dbe75438a166171
Jeff McCollum <jmccollum@hashicorp.com>
2022-09-22 14:16:54 -0500

    add tf version requirement and remove experimental

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     tfe = {
       source  = "hashicorp/tfe"
-      version = ">= 0.36.0"
+      version = ">= 0.36.1"
     }
-  }
-
-  experiments = [module_variable_optional_attrs]
+  } 
 }
