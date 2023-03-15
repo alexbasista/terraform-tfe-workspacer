52c180ea3dd01497b6e9a9f9117d8f70b3ab391c
Alex Basista <abasista@hashicorp.com>
2023-03-15 17:17:48 -0400

    Run Trigger datasource count fix (#14)
    * adjust count and for_each logic
    
    * bump provider version

--- run_triggers.tf
+++ run_triggers.tf
@@ -1,12 +1,12 @@
 data "tfe_workspace_ids" "run_triggers" {
-  count = var.run_trigger_source_workspaces == [] ? 0 : 1
+  count = length(var.run_trigger_source_workspaces) == 0 ? 0 : 1
 
   names        = var.run_trigger_source_workspaces
   organization = var.organization
 }
 
 resource "tfe_run_trigger" "rt" {
-  for_each = data.tfe_workspace_ids.run_triggers[0].ids
+  for_each = length(var.run_trigger_source_workspaces) == 0 ? {} : data.tfe_workspace_ids.run_triggers[0].ids
 
   workspace_id  = tfe_workspace.ws.id
   sourceable_id = each.value
