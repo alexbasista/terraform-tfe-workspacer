1fde878e42a7b5eb390e846e90d1a5b55dd12b35
alexbasista <abasista@hashicorp.com>
2023-05-09 10:40:43 -0400

    bump ver and docs

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.4.0"
 
   required_providers {
     tfe = {
       source  = "hashicorp/tfe"
-      version = ">= 0.42.0"
+      version = ">= 0.44.1"
     }
   } 
 }
