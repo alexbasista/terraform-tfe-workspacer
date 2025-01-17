07f85665c941755a608f4cc70a7733ec974d3d4c
alexbasista <abasista@hashicorp.com>
2025-01-17 08:49:39 -0500

    update test

--- tests/with-vcs/tf-working-dir-test/test_main.tf
+++ tests/with-vcs/tf-working-dir-test/test_main.tf
@@ -2,9 +2,11 @@ terraform {
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = "3.6.2"
+      version = "3.6.3"
     }
   }
 }
 
 resource "random_pet" "test_1" {}
+
+resource "random_pet" "test_2" {}
